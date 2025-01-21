99f38f489049c3aba0b0c160ddfa90aad6a9f7ed
Elizabeth Monsen <lizmonsen21@gmail.com>
2025-01-20 21:17:05 -0700

    Fig 2 and 3 add

--- app.py
+++ app.py
@@ -18,3 +18,31 @@ st.dataframe(vehicles_df)
 st.header('Number of Vehicles by Price and Type')
 fig1 = px.histogram(vehicles_df, x='type', color='price_range')
 st.write(fig1)
+
+st. header('Vehicle Price Based on Year and Condition')
+fig2 = px.scatter(vehicles_df, x='model_year', y='price', color='condition')
+fig2.update_layout(yaxis=dict(range=[0, 100000]))
+st.write(fig2)
+
+st.header('Compare Price Distribution Between Car Condidtions')
+condition_list = sorted(vehicles_df['condition'].unique())
+condition_1 = st.selectbox('Select condition 1',
+                              condition_list, index=condition_list.index('excellent'))
+
+condition_2 = st.selectbox('Select condition 2',
+                              condition_list, index=condition_list.index('good'))
+mask_filter = (vehicles_df['condition'] == condition_1) | (vehicles_df['condition'] == condition_2)
+df_filtered = vehicles_df[mask_filter]
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+st.write(px.histogram(df_filtered,
+                      x='price',
+                      nbins=50,
+                      range_x=[0, 100000],
+                      color='condition',
+                      histnorm=histnorm,
+                      barmode='overlay'))
+
